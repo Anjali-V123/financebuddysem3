f12127dbc15b7afdc5f2fc28c04b169179f7b55e
Anjali <ramaanjaliv5@gmail.com>
2024-11-19 20:55:09 +0530

    background image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Registration from './components/Registration';
+import Home from './components/Home';
+//import publicpic from "../public/publicpic.jpeg"
+ import financepic from "./financepic.jpeg"
+// import myImage from './financepic.jpeg';
+
+// const MyComponent = () => (
+//   <div style={{ backgroundImage: `url(${myImage})` }} />
+// );
+
 function App() {
   return (
     <div className="App">
+      {/* <img src={financepic}/> */}
+      <Home/>
       <Registration/>
     </div>
   );
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,18 @@
+import financepic from "../financepic.jpeg"
+import "../App.css"
+ function Home(){
+    const mystyle = {
+    backgroundColor:"red",
+    border:"1px solid green"
+    }
+    return(
+        <>
+        <div></div>
+       
+         {/* <img src={financepic}/> */}
+          {/* <div className={mystyle}>dsakjhd</div> */}
+        </>
+    )
+}
+
+export default Home
